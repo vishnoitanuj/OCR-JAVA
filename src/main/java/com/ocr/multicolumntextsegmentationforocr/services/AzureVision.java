017f2292c3ddeaa423383317cad0d97f3a8b2ac2
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2020-09-28 18:10:48 +0530

    Azure Vision API integrations

---
+++ src/main/java/com/ocr/multicolumntextsegmentationforocr/services/AzureVision.java
@@ -0,0 +1,119 @@
+package com.ocr.multicolumntextsegmentationforocr.services;
+
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.Header;
+import org.json.JSONObject;
+
+public class AzureVision {
+
+    private static String subscriptionKey = System.getenv("COMPUTER_VISION_SUBSCRIPTION_KEY");
+    private static String endpoint = System.getenv("COMPUTER_VISION_ENDPOINT");
+
+    private static String uriBase = endpoint + "/vision/v3.0/read/analyze";
+
+    private static String imageToAnalyze =
+            "https://raw.githubusercontent.com/MicrosoftDocs/azure-docs/master/articles/cognitive-services/Computer-vision/Images/readsample.jpg";
+
+    public static void main(String[] args) {
+        CloseableHttpClient httpTextClient = HttpClientBuilder.create().build();
+        CloseableHttpClient httpResultClient = HttpClientBuilder.create().build();;
+
+        System.out.println("Endpoint:         " + endpoint);
+        System.out.println("Subscription key: " + subscriptionKey);
+
+        try {
+            // This operation requires two REST API calls. One to submit the image
+            // for processing, the other to retrieve the text found in the image.
+
+            URIBuilder builder = new URIBuilder(uriBase);
+
+            // Prepare the URI for the REST API method.
+            URI uri = builder.build();
+            HttpPost request = new HttpPost(uri);
+
+            // Request headers.
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request body.
+            StringEntity requestEntity =
+                    new StringEntity("{\"url\":\"" + imageToAnalyze + "\"}");
+            request.setEntity(requestEntity);
+
+            // Two REST API methods are required to extract text.
+            // One method to submit the image for processing, the other method
+            // to retrieve the text found in the image.
+
+            // Call the first REST API method to detect the text.
+            HttpResponse response = httpTextClient.execute(request);
+
+            // Check for success.
+            if (response.getStatusLine().getStatusCode() != 202) {
+                // Format and display the JSON error message.
+                HttpEntity entity = response.getEntity();
+                String jsonString = EntityUtils.toString(entity);
+                JSONObject json = new JSONObject(jsonString);
+                System.out.println("Error:\n");
+                System.out.println(json.toString(2));
+                return;
+            }
+
+            // Store the URI of the second REST API method.
+            // This URI is where you can get the results of the first REST API method.
+            String operationLocation = null;
+
+            // The 'Operation-Location' response header value contains the URI for
+            // the second REST API method.
+            Header[] responseHeaders = response.getAllHeaders();
+            for (Header header : responseHeaders) {
+                if (header.getName().equals("Operation-Location")) {
+                    operationLocation = header.getValue();
+                    break;
+                }
+            }
+
+            if (operationLocation == null) {
+                System.out.println("\nError retrieving Operation-Location.\nExiting.");
+                System.exit(1);
+            }
+
+            // If the first REST API method completes successfully, the second
+            // REST API method retrieves the text written in the image.
+            //
+            // Note: The response may not be immediately available. Text
+            // recognition is an asynchronous operation that can take a variable
+            // amount of time depending on the length of the text.
+            // You may need to wait or retry this operation.
+
+            System.out.println("\nText submitted.\n" +
+                    "Waiting 10 seconds to retrieve the recognized text.\n");
+            Thread.sleep(10000);
+
+            // Call the second REST API method and get the response.
+            HttpGet resultRequest = new HttpGet(operationLocation);
+            resultRequest.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            HttpResponse resultResponse = httpResultClient.execute(resultRequest);
+            HttpEntity responseEntity = resultResponse.getEntity();
+
+            if (responseEntity != null) {
+                // Format and display the JSON response.
+                String jsonString = EntityUtils.toString(responseEntity);
+                JSONObject json = new JSONObject(jsonString);
+                System.out.println("Text recognition result response: \n");
+                System.out.println(json.toString(2));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
--- src/main/java/com/ocr/multicolumntextsegmentationforocr/services/GCPVision.java
+++ src/main/java/com/ocr/multicolumntextsegmentationforocr/services/GCPVision.java
@@ -2,17 +2,48 @@ package com.ocr.multicolumntextsegmentationforocr.services;
 
 import com.google.cloud.vision.v1.*;
 import com.google.protobuf.ByteString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gcp.vision.CloudVisionTemplate;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
+@Service
 public class GCPVision {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Autowired
+    private CloudVisionTemplate cloudVisionTemplate;
+
+//    public static void main(String... args) throws Exception {
+//        // Instantiates a client
+//        Storage storage = StorageOptions.getDefaultInstance().getService();
+//
+//        // The name for the new bucket
+//        String bucketName = "tanuj-bucket";  // "my-new-bucket";
+//
+//        // Creates the new bucket
+//        Bucket bucket = storage.create(BucketInfo.of(bucketName));
+//
+//        System.out.printf("Bucket %s created.%n", bucket.getName());
+//    }
+//
+//    public String getExtraction(Resource imageResource) {
+//        String textFromImage = cloudVisionTemplate.extractTextFromImage(imageResource);
+//        return textFromImage;
+//    }
 
     public static void main(String[] args) throws IOException {
         try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
 
-            String filePath = "src\\main\\resources\\handwritten.jpg";
+            String filePath = "src\\main\\resources\\number-handwritten.jpg";
             List<AnnotateImageRequest> requests = new ArrayList<>();
 
             ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
@@ -40,32 +71,32 @@ public class GCPVision {
 
                     // For full list of available annotations, see http://g.co/cloud/vision/docs
                     TextAnnotation annotation = res.getFullTextAnnotation();
-                    for (Page page : annotation.getPagesList()) {
-                        String pageText = "";
-                        for (Block block : page.getBlocksList()) {
-                            String blockText = "";
-                            for (Paragraph para : block.getParagraphsList()) {
-                                String paraText = "";
-                                for (Word word : para.getWordsList()) {
-                                    String wordText = "";
-                                    for (Symbol symbol : word.getSymbolsList()) {
-                                        wordText = wordText + symbol.getText();
-                                        System.out.format(
-                                                "Symbol text: %s (confidence: %f)%n",
-                                                symbol.getText(), symbol.getConfidence());
-                                    }
-                                    System.out.format(
-                                            "Word text: %s (confidence: %f)%n%n", wordText, word.getConfidence());
-                                    paraText = String.format("%s %s", paraText, wordText);
-                                }
-                                // Output Example using Paragraph:
-                                System.out.println("%nParagraph: %n" + paraText);
-                                System.out.format("Paragraph Confidence: %f%n", para.getConfidence());
-                                blockText = blockText + paraText;
-                            }
-                            pageText = pageText + blockText;
-                        }
-                    }
+//                    for (Page page : annotation.getPagesList()) {
+//                        String pageText = "";
+//                        for (Block block : page.getBlocksList()) {
+//                            String blockText = "";
+//                            for (Paragraph para : block.getParagraphsList()) {
+//                                String paraText = "";
+//                                for (Word word : para.getWordsList()) {
+//                                    String wordText = "";
+//                                    for (Symbol symbol : word.getSymbolsList()) {
+//                                        wordText = wordText + symbol.getText();
+//                                        System.out.format(
+//                                                "Symbol text: %s (confidence: %f)%n",
+//                                                symbol.getText(), symbol.getConfidence());
+//                                    }
+//                                    System.out.format(
+//                                            "Word text: %s (confidence: %f)%n%n", wordText, word.getConfidence());
+//                                    paraText = String.format("%s %s", paraText, wordText);
+//                                }
+//                                // Output Example using Paragraph:
+//                                System.out.println("%nParagraph: %n" + paraText);
+//                                System.out.format("Paragraph Confidence: %f%n", para.getConfidence());
+//                                blockText = blockText + paraText;
+//                            }
+//                            pageText = pageText + blockText;
+//                        }
+//                    }
                     System.out.println("%nComplete annotation:");
                     System.out.println(annotation.getText());
                 }
